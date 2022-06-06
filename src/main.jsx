02a229d94a650f85a0f41cffa3ecf9c3adc3a4ea
Alicia Padrón <aliciaapadron@gmail.com>
2022-06-06 19:24:08 +0200

    render list

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { App } from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
-)
+  </HashRouter>
+);

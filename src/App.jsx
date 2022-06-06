ad2a3282a026991fa4430a1e4ee9aad1126a0cdf
Alicia Padrón <aliciaapadron@gmail.com>
2022-06-06 20:48:29 +0200

    add package Chackra and uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
+import { Heading } from '@chakra-ui/react';
 import * as API from './services/launches';
+import logo from './assets/logo-spacex.png';
+import { v4 as uuidv4 } from 'uuid';
 
 export function App() {
   const [launches, setLaunches] = useState([]);
@@ -9,14 +12,17 @@ export function App() {
   }, []);
   return (
     <>
-      <h1>SpaceX Launches</h1>
-      <section>
+      <img src={logo} alt="logo de spaceX" width={300} />
+      <Heading as="h1" size="lg">
+        SpaceX Launches
+      </Heading>
+      <ul>
         {launches.map((launch) => (
-          <li key={launch.flight_number}>
+          <li key={uuidv4()}>
             {launch.mission_name}({launch.launch_year})
           </li>
         ))}
-      </section>
+      </ul>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <HashRouter>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </HashRouter>
 );
